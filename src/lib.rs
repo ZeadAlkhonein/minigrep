da8452ef94dd7120aa158b9b36a22317ddad9d05
ziyad alkhonein <ziyadalkhonein@gmail.com>
2025-01-05 23:14:32 +0300

    add search functionality to the tool

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 // src/lib.rs
-
+use std::process;
+use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io;
+
 
 /// Reads the entire contents of a file into a String.
 ///
@@ -20,3 +22,91 @@ pub fn read_file(filename: &str) -> Result<String, io::Error> {
     Ok(contents)
 }
 
+
+
+pub fn run(config: Config) -> Result<(), &'static str> {
+    // Attempt to read the contents of the file specified by config.file_path
+    match read_file(&config.file_path) {
+        Ok(contents) => {
+            // If the file is successfully read, print its contents to stdout
+            // println!("The file has the following contents:\n{contents}");
+            // Return Ok(()) to signify successful execution of the function
+            // let vec = search(&config.query, &contents);
+
+            // if vec.is_empty() {
+            //     println!("The vector is empty.");
+            // } else {
+            //     println!("The vector is not empty.");
+            // }
+
+            for line in search(&config.query, &contents) {
+                println!("{line}");
+            }
+
+            Ok(())
+        },
+        Err(e) => {
+            // If reading the file fails, print an error message to stderr
+            eprintln!("Failed to read file: {}", e);
+            // Exit the program with a failure status code
+            // Note: Using `process::exit(1)` here will terminate the program immediately, 
+            // which might not be ideal in all contexts (e.g., if this function was part of a larger system).
+            // An alternative could be to return `Err("Failed to read file")` or similar.
+            process::exit(1);
+        }
+    }
+}
+
+pub struct Config {
+    query: String,
+    file_path: String,
+
+}
+
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, Box<dyn Error>>{
+
+        if args.len() < 3 {
+            // eprintln!("Not enough arguments provided");
+            // process::exit(1);
+            return Err("Not enough arguments provided".into());
+        }
+    
+        let query: String = args[1].to_string();
+        let file_path: String = args[2].to_string();
+        Ok(Config { query, file_path })
+        }
+}
+
+pub fn search<'a>( query : &str, content:  &'a str)-> Vec<&'a str>{
+    let mut words : Vec<&str> = Vec::new();
+
+    for word in content.lines() {
+        // print!("{}", word);
+        if word.contains(query) {
+            words.push(&word);
+        }
+        else {
+            continue;
+        }
+    }
+    words
+
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "safe, fast, productive.";
+        let contents = "\
+        Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,61 +3,28 @@ use std::error::Error;
 use std::process;
 
 mod lib;
+use crate::lib::Config;
+use crate::lib::run;
+
+
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
 
-
-    // let (query, file_path) = parse_config(&args);
     
     let config: Config = Config::build(&args).unwrap_or_else(|err|{
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    run(config);
+    let _ = run(config)?;
+
+    // another way to handle error 
+    //     if let Err(e) = run(config) {
+    //     println!("Application error: {e}");
+    //     process::exit(1);
+    // }
 
     Ok(())
 }
-
-fn run(config : Config) -> Result<(), &'static str>{
-    match lib::read_file(&config.file_path) {
-        Ok(contents) => {
-            println!("The file has the following contents:\n{contents}");
-        Ok(())},
-        Err(e) => {
-            eprintln!("Failed to read file: {}", e);
-            process::exit(1);
-        }
-    }
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-
-}
-
-
-impl Config {
-    pub fn build(args: &[String]) -> Result<Config, Box<dyn Error>>{
-
-        if args.len() < 3 {
-            // eprintln!("Not enough arguments provided");
-            // process::exit(1);
-            return Err("Not enough arguments provided".into());
-        }
-    
-        let query: String = args[1].to_string();
-        let file_path: String = args[2].to_string();
-        Ok(Config { query, file_path })
-        }
-}
-
-// fn parse_config(args: &[String]) -> Config {
-//     let query: String = args[1].to_string();
-//     let file_path: String = args[2].to_string();
-
-//     Config { query, file_path }
-// }
